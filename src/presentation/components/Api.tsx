37cc5cbf918518eb3dd46bb5c452bd073594e909
Yesler2113 <diazmunozdm12@gmail.com>
2024-08-02 02:29:11 -0600

    Cambio en Api tsx

--- src/presentation/components/Api.tsx
+++ src/presentation/components/Api.tsx
@@ -1,64 +1,9 @@
-/*import axios from 'axios';
-import { Alert } from 'react-native';
-
-
-const API_BASE_URL = 'https://192.168.58.112:5000/api/photo';
-
-export const savePhoto = async (photo: any, description: string) => {
-
-  try {
-    const formData = new FormData();
-    formData.append('file', {
-      uri: photo.uri,
-      type: 'image/jpeg',
-      name: 'photo.jpg',
-    });
-    formData.append('description', description);
-
-    const response = await axios.post(`${API_BASE_URL}/guardar`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-     
-    });
-    return response.data;
-  } catch (error) {
-    if(axios.isAxiosError(error)){
-      console.error('Axios error details:',{
-        message: error.message,
-        code: error.code,
-        config: error.config,
-        response: error.config,
-      })
-    }
-  }
-};
-
-
-export const getPhotos = async () => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/photos`, {
-    });
-    return response.data;
-  } catch (error) {
-    if(axios.isAxiosError(error)){
-      console.error('Axios error details:',{
-        message: error.message,
-        code: error.code,
-        config: error.config,
-        response: error.config,
-      })
-    }
-  }
-}
-*/
-// Api.tsx
 import axios from 'axios';
 import { Alert } from 'react-native';
 
 const API_BASE_URL = 'http://192.168.58.112:5137/api/photo';
 
-// Definiciones de interfaces para la respuesta de la API
+
 interface PhotoDto {
   id: string;
   description: string;
